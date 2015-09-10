999b6085b9aa3d604b97839f5a58c6ccbcf86686
ironholds <ironholds@gmail.com>
2015-09-10 19:00:14 -0400

    C++ify pystr_center

--- R/pystr_center.R
+++ R/pystr_center.R
@@ -5,7 +5,7 @@
 #' @details Padding is done using the specified \code{fillchar} (default is an ASCII space).
 #' The original string is returned if \code{width} is less than or equal to \code{nchar(str)}.
 #'
-#' @param str A string.
+#' @param str A string or vector of strings.
 #' @param width An integer.
 #' @param fillchar A character.
 #'
@@ -21,17 +21,5 @@
 #'
 #' @export
 pystr_center <- function(str, width, fillchar=" ") {
-  if(width <= nchar(str)) {
-    return(str)
-  }
-
-  spaces_to_fill = width - nchar(str)
-
-  rightsize = ceiling(spaces_to_fill / 2)
-  leftsize = spaces_to_fill - rightsize
-
-  rightfill = paste0(rep(fillchar, rightsize), collapse="")
-  leftfill = paste0(rep(fillchar, leftsize), collapse="")
-
-  return(paste0(leftfill, str, rightfill))
+  return(pystr_center_(str, width, fillchar))
 }
---
+++ src/pystr_center.cpp
@@ -0,0 +1,34 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+std::string center_single(std::string initial_string, int& width, char& fill_char){
+
+  //If the intended width is less than or equal to the length of the initial string, return that string.
+  if(width <= initial_string.size()){
+    return initial_string;
+  }
+
+  //Calculate how many characters in each place
+  int delta = (width - initial_string.size());
+  int pre_initial = (int) ceil(delta/2);
+  int post_initial = (delta - pre_initial);
+
+  initial_string.insert(0, pre_initial, fill_char);
+  initial_string.insert(initial_string.size(), post_initial, fill_char);
+
+  return initial_string;
+
+}
+
+// [[Rcpp::export]]
+std::vector < std::string > pystr_center_(std::vector < std::string > strs, int width, char fillchar){
+
+  int& width_ref = width;
+  char& fill_ref = fillchar;
+
+  for(unsigned int i = 0; i < strs.size(); i++){
+    strs[i] = center_single(strs[i], width_ref, fill_ref);
+  }
+
+  return strs;
+}
